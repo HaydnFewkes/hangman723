00815aa12896568f991a36d5bc1c2d224cdfccb3
Haydn Fewkes <haydnsfewkes@outlook.com>
2024-01-03 12:53:02 +0000

    Updating files

--- milestone_2.py
+++ milestone_2.py
@@ -1,7 +1,10 @@
+# File for milestone_2, takes a list of words and selects a random one
 import random
 
+# Choosing a random word from the list
 word_list = ["Banana", "Pear", "Kiwi", "Peach", "Grape"]
 word = random.choice(word_list)
+# Takes a userinput for a single letter
 guess = input("Enter a single letter: ")
 if len(guess) == 1:
     print("Good Guess!")
